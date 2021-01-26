9729d0a73e972737e711f8de84fac2a575769e71
susanee <comtrial97@gmail.com>
2021-01-26 14:21:25 +0900

    add profile

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /** Site MetaData (Required all)*/
-  title: `Title`,                           // (* Required)
-  description: `Blog Description`,          // (* Required)
+  title: `susan's blog`,                           // (* Required)
+  description: `기록을 남기고 뭐 거창한 이것 저것 적고 이게 좀 컷으면 좋겠는데 `,          // (* Required)
   author: `Author`,                         // (* Required)
   language: 'ko-KR',                        // (* Required) html lang, ex. 'en' | 'en-US' | 'ko' | 'ko-KR' | ...
   siteUrl: 'https://',                      // (* Required)
